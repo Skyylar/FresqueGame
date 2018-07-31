328150e9161c62601175849469d6eb6cf6824ab4
Unknown <bailli_k@etna-alternance.net>
2018-07-31 21:36:18 +0200

    Update frise/puzzle

---
+++ Assets/Scripts/frise/ClickOnBox.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickOnBox : MonoBehaviour {
+
+    public Transform box;
+    private string name;
+	// Use this for initialization
+	void Start () {
+        name = box.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Start_" + name);
+        Type type = this.GetType();
+        MethodInfo call = type.GetMethod("Start_"+name);
+        call.Invoke(this, new object[] { });
+    }
+
+    //Start prehistoire
+    public void Start_Prehistoire()
+    {
+        Debug.Log("Vous avez cliqué sur le bouton prehistoire.");
+        SceneManager.LoadScene("FindObject", LoadSceneMode.Additive);
+    }
+
+    //Start antiquite
+    public void Start_Antiquite()
+    {
+        Debug.Log("Vous avez cliqué sur le bouton antiquite.");
+        SceneManager.LoadScene("Puzzle", LoadSceneMode.Additive);
+    }
+
+    //Start moyen-age
+    public void Start_MoyenAge()
+    {
+        Debug.Log("Vous avez cliqué sur le bouton moyen-age.");
+        SceneManager.LoadScene("WordGame", LoadSceneMode.Additive);
+    }
+
+    //Start temps modernes
+    public void Start_TempsModernes()
+    {
+        Debug.Log("Vous avez cliqué sur le bouton temps modernes.");
+        SceneManager.LoadScene("quiz", LoadSceneMode.Additive);
+    }
+
+    //Start XIXeme siecle
+    public void Start_XIXSiecle()
+    {
+        Debug.Log("Vous avez cliqué sur le bouton XIXeme siecle.");
+        //SceneManager.LoadScene("XIX_siecle", LoadSceneMode.Single);
+    }
+
+    //Start XXeme siecle
+    public void Start_XXSiecle()
+    {
+        Debug.Log("Vous avez cliqué sur le bouton XXeme siecle.");
+        //SceneManager.LoadScene("XX_siecle", LoadSceneMode.Single);
+    }
+}
