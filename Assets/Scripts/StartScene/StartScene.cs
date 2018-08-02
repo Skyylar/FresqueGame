16ecfcfb56eff85809887e78c6c1ae413988193c
Gyrotak <lejaul_w@etna-alternance.net>
2018-08-02 14:54:40 +0200

    [StartGames] Add start game page

---
+++ Assets/Scripts/StartScene/StartScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour 
+{
+
+    // Start Games function
+    public void startGames() => SceneManager.LoadScene("friscene", LoadSceneMode.Single);
+
+}
