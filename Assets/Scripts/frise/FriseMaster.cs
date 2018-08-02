f3716a35ed6384f3c3cfc3e0d1d38c302708c317
Unknown <bailli_k@etna-alternance.net>
2018-08-02 10:31:15 +0200

    Clean frise

--- Assets/Scripts/frise/FriseMaster.cs
+++ Assets/Scripts/frise/FriseMaster.cs
@@ -19,29 +19,21 @@ public class FriseMaster : MonoBehaviour {
         if (GameManager.NoteFindObject != 0)
         {
             GameObject.Find("Prehistoire").GetComponent<BoxCollider>().enabled = false;
-            GameObject.Find("Prehistoire").GetComponent<MeshCollider>().enabled = false;
-            GameObject.Find("Prehistoire").GetComponent<Button>().enabled = false;
             GameObject.Find("Prehistoire").GetComponent<ClickOnBox>().enabled = false;
         }
         if (GameManager.NoteQuiz != 0)
         {
             GameObject.Find("TempsModernes").GetComponent<BoxCollider>().enabled = false;
-            GameObject.Find("TempsModernes").GetComponent<MeshCollider>().enabled = false;
-            GameObject.Find("TempsModernes").GetComponent<Button>().enabled = false;
             GameObject.Find("TempsModernes").GetComponent<ClickOnBox>().enabled = false;
         }
         if (GameManager.NotePuzzle != 0)
         {
             GameObject.Find("Antiquite").GetComponent<BoxCollider>().enabled = false;
-            GameObject.Find("Antiquite").GetComponent<MeshCollider>().enabled = false;
-            GameObject.Find("Antiquite").GetComponent<Button>().enabled = false;
             GameObject.Find("Antiquite").GetComponent<ClickOnBox>().enabled = false;
         }
         if (GameManager.NoteMeliMelo != 0)
         {
             GameObject.Find("MoyenAge").GetComponent<BoxCollider>().enabled = false;
-            GameObject.Find("MoyenAge").GetComponent<MeshCollider>().enabled = false;
-            GameObject.Find("MoyenAge").GetComponent<Button>().enabled = false;
             GameObject.Find("MoyenAge").GetComponent<ClickOnBox>().enabled = false;
         }
 	}
